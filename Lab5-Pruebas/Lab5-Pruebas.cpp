3b22ed9fa8987ef07ef28acfe3d95c2677a48dd1
JOSE HERNANDEZ MENDOZA <jose.hernandez.mendoza@est.una.ac.cr>
2025-10-29 18:52:11 -0600

    circulo

--- Lab5-Pruebas/Lab5-Pruebas.cpp
+++ Lab5-Pruebas/Lab5-Pruebas.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "Shape.h"
 #include "Square.h"
+#include "Circle.h"
 
 int main()
 {
 
     Shape* soyUnaFigura = new Square(2.0);
+    Shape* soyOtraFigura = new Square(2.0);
 
-    double resultado = soyUnaFigura->calculateArea();
+    double resultadoCuadro = soyUnaFigura->calculateArea();
+    double resultadoCirculo = soyOtraFigura->calculateArea();
+
     std::cout << "Hola, el lado de un cuadrado de base 2, es:\n";
-	std::cout << resultado << std::endl;
+	std::cout << resultadoCuadro << std::endl;
+
+    std::cout << "Hola, el area de un circulo de radio 2, es:\n";
+    std::cout << resultadoCirculo << std::endl;
 }
+
